219e335999a1d6f87ad7c9240fd950d9e9dfc494
Rouven <rouven@MacBook-Pro-2.local>
2018-10-22 10:25:12 +0200

    In der snake.js wurden folgende Logiken implementiert. Eine Methode "beende Spiel" beendet das Spiel in dem alle Variablen auf die Start-Werte gesetzt und das Spielfeld gelöscht. Darüber hinaus wurden die Methoden plusGeschwindikeit und minusgeschwindigkeit implementiert was den Buttons Logik hinterlegt. Hier werden über die Var verzögerung jeweils entsprechend neue Werte gesetzt. Eine Spielfeldumrandung wurde zur optischen Aufwertung hinzugefügt. "Wenn der Apfel gegessen wurde" wurde der Spiele Methode hinzugefügt. Darüber hinaus wurde hinzugefügt wenn die Schlange den Rand berührt. Hierbei wird das Spiel beendet und die Methode "beende Spiel" ausgeführt. Zuletzt wird der Highscore dem aktuellen Spielverlauf angepasst.

--- snake.js
+++ snake.js
@@ -12,8 +12,8 @@ let context = null;
 let refreshId = null;
 
 //Variablen für die Schlange
-let kopfX = Math.floor(spielfeld/2);
-let kopfY = Math.floor(spielfeld/2);
+let kopfX = Math.floor(spielfeld / 2);
+let kopfY = Math.floor(spielfeld / 2);
 let dicke = 10;
 let schritte = 10;
 let laufeX = -schritte;
@@ -45,10 +45,39 @@ window.addEventListener("load", () => {
   //add EventListener to the button stopp
   document.getElementById("stopp").addEventListener("click", beendeSpiel);
 
+  //add EventListener to the button plus
+  document.getElementById("plus").addEventListener("click", plusGeschwindigkeit);
 
-  });
+  //add EventListener to the button minus
+  document.getElementById("minus").addEventListener("click", minusGeschwindigkeit);
+});
 
 let beendeSpiel = () => {
+  //Verberge den Stopp-Knopf und zeige den Start-Knopf
+  document.getElementById("stopp").style.display = "none";
+  document.getElementById("start").style.display = "inline";
+
+  //zeige das Schlangen-Bild
+  document.getElementById("snakePreStart").style.display = "inline";
+
+  //Beende das Spiel
+  context.fillStyle = "white";
+  context.fillRect(0,0,spielfeld,spielfeld);
+  canva = null;
+  context = null;
+
+  //Setzte die Variablen auf den intialen Wert
+  kopfX = spielfeld/2;
+  kopfY = spielfeld/2;
+  laenge = startLaenge;
+  schritte = 10;
+  laufeX = -schritte;
+  laufeY = 0;
+  schwanz = [];
+
+  //refresche das Canvas Element
+  window.cancelAnimationFrame(refreshId);
+
 }
 
 let starteSpiel = () => {
@@ -68,9 +97,9 @@ let starteSpiel = () => {
   refreshId = window.requestAnimationFrame(spiele);
 
   //solange der Apfel nicht innerhalb des Spielfeldes ist, muss er neu positioniert werden
-  while(apfelX < 10 || apfelX >= spielfeld - 10 || apfelY < 10 || apfelY >= spielfeld -10){
-  apfelX = Math.floor(Math.random()*spielfeld);
-  apfelY = Math.floor(Math.random()*spielfeld);
+  while (apfelX < 10 || apfelX >= spielfeld - 10 || apfelY < 10 || apfelY >= spielfeld - 10) {
+    apfelX = Math.floor(Math.random() * spielfeld);
+    apfelY = Math.floor(Math.random() * spielfeld);
   }
 }
 
@@ -85,30 +114,60 @@ let spiele = () => {
 
     // Logik des Spiels:
 
-	//Wohin läuft die Schlange
+    //Wohin läuft die Schlange
     kopfX += laufeX;
     kopfY += laufeY;
 
     //schwarzer Hintergrund
     context.fillStyle = "black";
-    context.fillRect(0,0,spielfeld,spielfeld);
+    context.fillRect(0, 0, spielfeld, spielfeld);
+
+    //Umrandung
+    context.fillStyle = "rgb(108, 47, 7)";
+    context.fillRect(0,0,spielfeld,10);
+    context.fillStyle = "rgb(108, 47, 7)";
+    context.fillRect(0,0,10,spielfeld);
+    context.fillStyle = "rgb(108, 47, 7)";
+    context.fillRect(0,spielfeld-10,spielfeld,spielfeld);
+    context.fillStyle = "rgb(108, 47, 7)";
+    context.fillRect(spielfeld-10,0,spielfeld,spielfeld);
 
     //Schlange
     context.fillStyle = "lime";
-    for(let i=0; i < schwanz.length; i++){
-      context.fillRect(schwanz[i].x,schwanz[i].y,dicke,dicke);
+    for (let i = 0; i < schwanz.length; i++) {
+      context.fillRect(schwanz[i].x, schwanz[i].y, dicke, dicke);
       //Falls der Kopf sich selbst berührt
-      if(schwanz[i].x === kopfX && schwanz[i].y == kopfY){
+      if (schwanz[i].x === kopfX && schwanz[i].y == kopfY) {
         laenge = startLaenge;
       }
     }
 
     //füege aktuellen Kopf dem Schwanz hinzu
-    schwanz.push({x:kopfX,y:kopfY});
+    schwanz.push({
+      x: kopfX,
+      y: kopfY
+    });
+
     //Falls der Schwanz länger ist als die Länge, lösche ein Element
-    while(schwanz.length > laenge){
+    while (schwanz.length > laenge) {
       schwanz.shift();
     }
+
+    //wenn der Apfel gegessen wurde
+  if(apfelX === kopfX && apfelY === kopfY){
+  //Update die Länge der Schlange
+    laenge++;
+    //Apfel erst auf 0 gesetzt, dass ein neuer zufälliger Wert gewürfelt wird
+    //Eine While-Schleife, damit falls der Apfel zufällig im Rahmen liegt,
+    // er neu ermittelt wird
+    apfelX = 0;
+    apfelY = 0;
+    while(apfelX < 10 || apfelX >= spielfeld - 10 || apfelY < 10 || apfelY >= spielfeld -10){
+      apfelX = Math.floor(Math.random()*spielfeld);
+      apfelY = Math.floor(Math.random()*spielfeld);
+
+    }
+  }
 
     //Apfel zeichnen:
     context.fillStyle = "red";
@@ -116,31 +175,70 @@ let spiele = () => {
 
   }
 
+  //Die Anzeige der Länge aktualisieren
+document.getElementById("laenge").innerHTML = laenge;
+
+//Setzte highscore
+if(laenge >= highscore) {
+highscore = laenge;
+}
+let hAlt = parseInt(document.getElementById("highscore").innerHTML);
+
+if(hAlt < highscore){
+  document.getElementById("highscore").innerHTML = highscore;
+}
+
+// Snake darf den Rand nicht berühren
+if(kopfX < 10) {
+beendeSpiel();
+}
+if(kopfX >= spielfeld-10) {
+beendeSpiel();
+}
+if(kopfY < 10){
+beendeSpiel();
+}
+if(kopfY >= spielfeld -10){
+beendeSpiel();
+}
+
   //rufe die Funktion wieder auf
   refreshId = window.requestAnimationFrame(spiele);
 }
 
 let wechsleRichtung = (e) => {
-  switch(e.keyCode){
+  switch (e.keyCode) {
     //Pfeiltaste nach links
     case 37:
       laufeX = -schritte;
       laufeY = 0;
       break;
-    //Pfeiltaste nach oben
+      //Pfeiltaste nach oben
     case 38:
       laufeX = 0;
       laufeY = -schritte;
       break;
-    //Pfeiltaste nach rechts
+      //Pfeiltaste nach rechts
     case 39:
       laufeX = schritte;
       laufeY = 0;
       break;
-    //Pfeiltaste nach unten
+      //Pfeiltaste nach unten
     case 40:
       laufeX = 0;
       laufeY = schritte;
       break;
   }
+}
+
+let plusGeschwindigkeit = () => {
+  verzögerung = Math.max(Math.min(60, verzögerung - 10), 20);
+  window.cancelAnimationFrame(refreshId);
+  refreshId = window.requestAnimationFrame(spiele);
+}
+
+let minusGeschwindigkeit = () => {
+  verzögerung = Math.max(Math.min(60, verzögerung + 10), 20);
+  window.cancelAnimationFrame(refreshId);
+  refreshId = window.requestAnimationFrame(spiele);
 }
